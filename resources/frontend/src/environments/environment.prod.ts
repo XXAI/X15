7a63bb774e4770dede2e6e71181611f4414eb52c
goraider <alejandro_gosain@hotmail.com>
2022-01-18 13:04:38 -0600

    build a produccion to https

--- resources/frontend/src/environments/environment.prod.ts
+++ resources/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  base_url: 'http://donadores.saludchiapas.gob.mx/api',
+  base_url: 'https://donadores.saludchiapas.gob.mx/api',
   images_url: '/images/'
 };
